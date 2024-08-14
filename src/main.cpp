04278b6560dd5656414bdb1ec2df231db02709f1
Boyuan Shi <121869789+Boyuan-Shi-0607@users.noreply.github.com>
2024-08-14 17:25:43 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
 #include "../include/greedy_cross.h"
 #include "../utility/change_vector_type.h"
+#include "../tests/test_functions.h"
+#include <Eigen/Dense>
 #include <cmath>
-
-double func(std::vector<int> y) {
-	std::vector<double> x = change_vector_type(y);
-	return std::exp(x[0] * std::cos(x[1] / dim[1] + x[2] / dim[2]) / (static_cast<double>(dim[0]) / 10));
-	// return std::cos(static_cast<double>(x[1]) / dim[1] + static_cast<double>(x[2]) / dim[2] + static_cast<double>(x[0]) / dim[0]);
-}
+#include <cstdlib>
+#include <ctime>
 
 int main () {
+	/*
 	dim = {1000, 50, 50};
 	n_swp = 10;
-	tol = {1e-7, 1e-7, 1e-7};
+	tol = {1e-1, 1e-1, 1e-1};
 	verbose = true;
 	cross_data results = greedy_cross(func);
-	std::vector<double> eps = results.eps;
+	// std::vector<double> eps = results.eps;
+	 */
+	
+	std::random_device rd_mat;
+	std::mt19937 gen_mat(rd_mat());
+	std::uniform_real_distribution<double> dist_mat(-10., 10.);
+
+	for (int r = 0; r < rank; ++r) {
+		Eigen::VectorXd u(rows);
+		Eigen::VectorXd v(cols);
+		
+		for (int i = 0; i < rows; ++i) {
+			u(i) = dist_mat(gen_mat);
+		}
+		for (int j = 0; j < cols; ++j) {
+			v(j) = dist_mat(gen_mat);
+		}
+		matrix = u * v.transpose();
+	}
+	
+	dim = {20, 20};
+	n_swp = 10;
+	tol = {1e-10, 1e-10};
+	verbose = true;
+	cross_data results = greedy_cross(func_1);
 	return 0;
+	
 }
